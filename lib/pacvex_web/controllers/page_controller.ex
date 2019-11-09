e1f888df8991a80f01bd08c7f389383c895516a5
Pablo <Pablo.Palomino1@outlook.com>
2019-11-09 15:17:07 +0100

    Create home and contact routes and firts approach

--- lib/pacvex_web/controllers/page_controller.ex
+++ lib/pacvex_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule PacvexWeb.PageController do
   use PacvexWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "home.html")
+  end
+
+  def contact(conn, _params) do
+    render(conn, "contact.html")
   end
 end
