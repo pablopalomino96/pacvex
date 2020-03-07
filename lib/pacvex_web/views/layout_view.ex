514a4230554d1ab9c31faa25bca5dedefef8eea6
pablopalomino96 <pablo.palomino1@outlook.com>
2020-03-07 14:11:37 +0100

    Add favicon and custom page title

--- lib/pacvex_web/views/layout_view.ex
+++ lib/pacvex_web/views/layout_view.ex
@@ -1,3 +1,18 @@
 defmodule PacvexWeb.LayoutView do
   use PacvexWeb, :view
+
+  def format_page_title(name) do
+    name
+    |> String.split()
+    |> case do
+      [name] -> name
+      [first_name | lastnames] -> "#{first_name} #{format_lastname(lastnames)}"
+    end
+  end
+
+  defp format_lastname([lastname]), do: String.first(lastname)
+
+  defp format_lastname([lastname | rest]) do
+    "#{String.first(lastname)}#{format_lastname(rest)}"
+  end
 end
